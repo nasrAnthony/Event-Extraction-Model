d02883ee6789501b12c41a502f4bb566e8dce864
Yusuf Hilal <yusufyh02@gmail.com>
2026-02-06 13:54:30 -0500

    made classifier_model.py functional but still needs tweaks to text features

--- models/classifier_model.py
+++ models/classifier_model.py
@@ -1,33 +1,47 @@
 import pandas as pd
+from pathlib import Path
 from catboost import CatBoostClassifier
-from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.model_selection import GroupShuffleSplit, train_test_split, cross_validate
 from sklearn.metrics import confusion_matrix, accuracy_score
 
-# loading data
-dataset = pd.read_csv("Data.csv") #@Yhilal02 adjust path for robustness
+ROOT = Path(__file__).resolve().parent.parent
+DATA = ROOT / "data"
 
-X = dataset.iloc[:, :-1]
-y = dataset.iloc[:, -1]
+# loading data -----------------------------------------------------------------
+# dataset = pd.read_csv("../data/full_data.csv") # for notebook
+dataset = pd.read_csv(DATA / "full_data.csv")
+dataset["is_event"] = dataset["event_id"].notna().astype(int)
 
-# data split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0) # will have to split by groups later
+cols = ["is_event", "source", "event_id", "label"] # cols not used for classifier
+X = dataset.drop(columns=cols, axis=1)
+y = dataset["is_event"]
 
-# catboost model training (very basic rn)
-# could do xgboost for speed 
-cat_cols = X_train.select_dtypes(include=["object"]).columns
-cat_idx = [X_train.columns.get_loc(c) for c in cat_cols]
+# train/test split by source ----------------------------------------------------
+gss = GroupShuffleSplit(n_splits=1, test_size=0.2)
 
-classifier = CatBoostClassifier(
-    task_type="GPU",
-    devices="0",
-    verbose=False
-)
-classifier.fit(X_train, y_train, cat_features=cat_idx)
+train_idx, test_idx = next(gss.split(X, y, groups=dataset["source"]))
 
-# test set prediction
+X_train, X_test = dataset.iloc[train_idx][X.columns], dataset.iloc[test_idx][X.columns]
+y_train, y_test = dataset.iloc[train_idx]["is_event"], dataset.iloc[test_idx]["is_event"]
+
+# # check to ensure the sources are separated
+# train_sources = set(dataset.iloc[train_idx]["source"])
+# test_sources  = set(dataset.iloc[test_idx]["source"])
+# print("overlap:", train_sources & test_sources)
+
+# CatBoost model-----------------------------------------------------------------
+cat_features = ["tag", "parent_tag"] # categorical features
+text_features = ["text_context", "attributes", "link"]
+
+classifier = CatBoostClassifier(iterations=1000, task_type="GPU", verbose=False)
+print("Starting Training..")
+classifier.fit(X_train,y_train,cat_features=cat_features)
+print("Training Done! Saving Model :)")
+classifier.save_model("catboost_event_classifier.cbm")
+
 y_pred = classifier.predict(X_test)
 
-# Metrics --------------------------------------------------
+# Metrics -----------------------------------------------------------------------
 # confusion matrix/accuracy score
 cm = confusion_matrix(y_test, y_pred)
 acc = accuracy_score(y_test, y_pred)
@@ -35,18 +49,16 @@ acc = accuracy_score(y_test, y_pred)
 print(cm)
 print(f"Accuracy: {acc:.2f}")
 
-# # cross validation (maybe for later, takes some time)
+# # cross validation (used in baseline test, revisit later)
 # cv_out = cross_validate(
 #     estimator=CatBoostClassifier(verbose=False),
 #     X=X_train,
 #     y=y_train,
-#     cv=3,  # small dataset
-#     params={"cat_features": cat_idx}
+#     cv=20
+#     params={"cat_features": cat_features}
 # )
 
 # scores = cv_out["test_score"]
 # print("Accuracy: {:.2f} %".format(scores.mean() * 100))
 # print("Standard Deviation: {:.2f} %".format(scores.std() * 100))
-
-
 
